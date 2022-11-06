7cffba938b8104737f73771e8dabc56b97fbd595
Nathan Jackson <nathan@csnate.com>
2022-11-06 14:49:39 -0500

    remove commented code from CMP instruction

--- arch/x86/semantics/cmp.c
+++ arch/x86/semantics/cmp.c
@@ -26,18 +26,4 @@ void x86_insn_cmp(struct x86_register_file* register_file, struct operand* lhs, 
     }
     PF(register_file) = (0 == (__builtin_popcount(lsb) % 2));
     AF(register_file) = (((BYTE(lhs) & 0xF) - (BYTE(rhs) & 0xF)) & 0xF0) > 0;
-//    if (sizeof(word) == lhs->size) {
-//        word delta = WORD(lhs) - WORD(rhs);
-//        CF(register_file) = __builtin_sub_overflow(WORD(lhs), WORD(rhs), &delta);
-//        OF(register_file) = __builtin_sub_overflow((int16_t) WORD(lhs), (int16_t) WORD(rhs), &delta);
-//        SF(register_file) = (delta & 0x8000);
-//        ZF(register_file) = (delta == 0);
-//    } else if (sizeof(byte) == lhs->size) {
-//        byte delta = BYTE(lhs) - BYTE(rhs);
-//        CF(register_file) = __builtin_sub_overflow(BYTE(lhs), BYTE(rhs), &delta);
-//        OF(register_file) = __builtin_sub_overflow((int8_t) BYTE(lhs), (int8_t) BYTE(rhs), &delta);
-//        SF(register_file) = (delta & 0x8000);
-//        ZF(register_file) = (delta == 0);
-//    }
-//    PF(register_file) = (0 == (__builtin_popcount(BYTE(lhs)) % 2));
 }
